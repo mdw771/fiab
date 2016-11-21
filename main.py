84a74ff848e1fbdbfa88a32553a55cdf7c6be96f
Ming <mingdu2015@u.northwestern.edu>
2016-11-21 09:55:31 -0600

    time

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from util import *
-import numpy as np
-from scipy.ndimage import imread
 from scipy.misc import imsave
-from scipy.signal import convolve2d
-import cv2
-from scipy.ndimage.filters import gaussian_filter
-import matplotlib.pyplot as plt
-from mpi4py import MPI
+import time
 
 
 img_ls = ['IMG_1158.jpg', 'IMG_1159.jpg', 'IMG_1160.jpg', 'IMG_1161.jpg']
 
 
+t0= time.time()
 pano = panorama(img_ls, n_pi=1000)
 pano_img = pano.build_panorama(0.65, interpolate=False)
 # pano.visualize_match(1)
 imsave('panorama.png', pano_img)
+print 'Done in {:.2f} s.'.format(time.time()-t0)
